612fc7d40a1a8b52beb30f34206ddfaa3cbae9d3
jmarzin1 <julien.marzin@lumiplan.com>
2025-06-19 10:48:14 +0200

    feat(DATA-1123): add cors configuration to get API status from ishtar

--- src/main/java/org/entur/kishar/routes/RestRouteBuilder.java
+++ src/main/java/org/entur/kishar/routes/RestRouteBuilder.java
@@ -28,6 +28,10 @@ public class RestRouteBuilder extends RouteBuilder {
     public void configure() {
 
         restConfiguration()
+                .enableCORS(true)
+                .corsAllowCredentials(true)
+                .corsHeaderProperty("Access-Control-Allow-Origin", "*")
+                .corsHeaderProperty("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, X-Gravitee-Api-Key")
                 .component("jetty")
                 .port(portNumber);
 
