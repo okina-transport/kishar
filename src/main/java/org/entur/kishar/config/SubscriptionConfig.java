fa7ae00a707c8bbc76b4e739206d706be3557d57
poswald <poswald@okina.fr>
2025-11-18 16:31:49 +0100

    fix: DATA-1431 problem updating idprocessing

--- src/main/java/org/entur/kishar/config/SubscriptionConfig.java
+++ src/main/java/org/entur/kishar/config/SubscriptionConfig.java
@@ -58,6 +58,16 @@ public class SubscriptionConfig {
                 idProcessingParameters.add(incomingParam);
             }
         }
+        idProcessingParameters.removeIf(cachedParam ->
+                !isPresentInIncoming(cachedParam, incomingParams)
+        );
+    }
+
+    private boolean isPresentInIncoming(IdProcessingParameters cachedParam, List<IdProcessingParameters> incomingParams) {
+        return incomingParams.stream().anyMatch(incoming ->
+                incoming.getDatasetId().equals(cachedParam.getDatasetId()) &&
+                        incoming.getObjectType().equals(cachedParam.getObjectType())
+        );
     }
 
     private Optional<IdProcessingParameters> getExistingIdProc(IdProcessingParameters incomingParam) {
